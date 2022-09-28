1dbab992a609dcc87305c1560e689d32c1e14879
JoseSolano20 <67623917+JoseSolano20@users.noreply.github.com>
2022-09-27 23:30:59 -0500

    dddd

---
+++ sw.js
@@ -0,0 +1,35 @@
+self.addEventListener('install',(event)=>{
+ console.log('sw: install');
+});
+
+self.addEventListener('fetch',(event)=>{
+ if (event.request.url.includes('style.css')){
+ console.log(event.request.url);
+ 
+ const resp = new Response (
+
+    `
+    body{
+
+            color: red;
+            background-color: #000;
+
+        }`,
+    {
+
+        headers: {
+            'Content-Type': 'text/css',
+        },
+    }  
+ );
+ }
+
+
+});
+
+self.addEventListener('fetch',(event)=>{
+    if(event.request.url.includes('animal1.jpg')){
+        let foto = fetch('/images/animal2.jpg');
+        event.respondWith(foto);
+    }
+});
